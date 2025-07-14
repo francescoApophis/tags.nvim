396190dd96a07d803b1a2d2ee942010d349da07f
Francesco <francescopaul789@gmail.com>
2025-07-14 03:56:08 +0200

    fix: (read msg) plugin build fails when installed from remote with Lazy
    The plugin first needs to find project's
    'base-folder', so that it can then locate the
    folder where the 'lib*parser.so' should
    be compiled.
    
    To find this it uses Lua's 'debug.getinfo()',
    which returns two kinds of paths,
    "source" and "short_src". I shuold have used
    the former, instead I used the ladder.
    
    That's because when getting the path,
    'debug.getinfo()' will truncate and prepend with '...'
    the returned path if too long.
    
    When the plugin gets installed from github
    through lazy, it gets by default installed
    in '.local/share/nvim/lazy/...':
    
    now the full path is too long and it's unusable,
    (has '...' prepended + is incomplete) which causes
    a linking error since it cannot find the path.

--- lua/tags/init.lua
+++ lua/tags/init.lua
@@ -11,9 +11,15 @@ local build_parser = function(lang)
 -- TODO: mention the github pull-request where we took this 
   -- https://github.com/nvim-lualine/lualine.nvim/compare/c5bd65d...c3fad10
   local sep = package.config:sub(1, 1)
-  local tags_path = debug.getinfo(1, "S").short_src:gsub('/[^/]+$', '') -- TODO: better name
+  local tags_folder_path = debug.getinfo(1, "S").source
 
-  local lib_path = table.concat({tags_path, 'parsers', lang}, sep)
+  if tags_folder_path:sub(1, 1) ~= '@' then
+    error('tags.nvim: failed to retrieve project folder path')
+  end
+
+  tags_folder_path = tags_folder_path:sub(2, -1):gsub('/[^/]+$', '')
+
+  local lib_path = table.concat({tags_folder_path, 'parsers', lang}, sep)
   local _lib_files_raw = api.nvim_exec2(fmt('!ls -p %s | grep -v / ', lib_path), {output = true}).output
   local lib_files = _lib_files_raw
                     :gsub('\n[^%.]+.so', '') -- ignore .so files
@@ -32,7 +38,7 @@ local build_parser = function(lang)
   local cmd_build = fmt('!cc -o %s %s  %s', lib_target, lib_files, cflags)
   local result = api.nvim_exec2(cmd_build, {output = true}).output
   if result:find('shell returned %d+') then -- TODO: does this handle all kinds of failure?
-    error('\n\n' .. result)
+    error('\n\n' .. 'tags.nvim:\n' .. result)
   end
 
   return lib_target
